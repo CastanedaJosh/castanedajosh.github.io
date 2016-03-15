9f33fc6dd68714badd4020754b1d53744773bdcd
Joe Pichardo <joepichar@gmail.com>
2016-03-15 01:34:15 -0700

    added smaller font and smartphone and tablet detection to prevent expand feature to execute because grid does calculate resize on mobile browsers

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,29 @@
-
 //once page loads the overlay will be removed
-window.addEventListener("load", function(){
+window.addEventListener("load", function() {
   var loadScreen = $("#overlay");
   loadScreen.remove();
 
 });
 
-
-
 $(document).ready(function() {
 
-   
-
-//add images to grid, files/images are listed as numbers
- /* for(var i = 1; i <= 23; i++){
-    $('.grid').append('<div class="grid-item"><img class="img" src="img/' + i + '.jpg"></div>');
-  }
-  */
-
+  //add images to grid, files/images are listed as numbers
+  /* for(var i = 1; i <= 23; i++){
+     $('.grid').append('<div class="grid-item"><img class="img" src="img/' + i + '.jpg"></div>');
+   }
+   */
 
   //next line is used after the img tag in previous append function to add title , materials used, and price under image
-//<p><span class="title">Title (Month/Year)</span><span>Materials Used</span><span class="price">$0000</span></p></div>
+  //<p><span class="title">Title (Month/Year)</span><span>Materials Used</span><span class="price">$0000</span></p></div>
 
   var gutterSize = 10;
 
   //change gutter size to prevent movement 
   function masonOff() {
 
-    if ($(window).width() <= 410 ) {
+    if ($(window).width() <= 410) {
 
-     $('.grid-item').width("100%");
+      $('.grid-item').width("100%");
 
     } else if ($(window).width() <= 450) {
       $('.grid-item').width("180px");
@@ -59,14 +53,10 @@ $(document).ready(function() {
 
     }
 
-  }//end of masonOff
-
-
+  } //end of masonOff
 
   //fixes overlap issues of pictures, waits until images loads to put them into the grid
   var $grid = $('.grid').imagesLoaded(function() {
-
-
 
     // init Masonry after all images have loaded
     $grid.masonry({
@@ -77,57 +67,16 @@ $(document).ready(function() {
       gutter: gutterSize
     });
 
-
-
     //helps with overlapping issue on devices under 400px
     $grid.masonry();
     masonOff();
     $grid.masonry();
-
-
-//testing loading of images, without images in html
-/*
-     var $items = getItems();
-    masonryImagesReveal( $items );
-  
-
- function masonryImagesReveal( $items ) {
-  var msnry = $grid.data('masonry');
-  var itemSelector = msnry.options.itemSelector;
-  // hide by default
-  $items.hide();
-  // append to container
-  $grid.append( $items );
-  $items.imagesLoaded().progress( function( imgLoad, image ) {
-    // get item
-    // image is imagesLoaded class, not <img>, <img> is image.img
-    var $item = $( image.img ).parents( itemSelector );
-    // un-hide item
-    $item.show();
-    // masonry does its thing
-    msnry.appended( $item );
-  });
-  
-  return this;
-};
-
-function getItems() {
-  var items = '';
-  for ( var i=1; i <= 23; i++ ) {
-    items += '<div class="grid-item"><img class="img" src="img/' + i + '.jpg"></div>';
-  }
-  // return jQuery object
-  return $( items );
-}
-*/
-//last of loading of images
 
     //checks window size settings when window is resized
     $(window).resize(function() {
 
       masonOff();
 
-      
       //checks box size when user resizes window
       if ($('.box').height() > $(window).height() || $('.box').width() > $(window).width()) {
 
@@ -141,117 +90,117 @@ function getItems() {
 
   }); //end of images loaded
 
-      $(window).on("orientationchange",function(){
-         $grid.masonry();
-          masonOff();
+  $(window).on("orientationchange", function() {
+    $grid.masonry();
+    masonOff();
 
-      
-      //checks box size when user resizes window
-      if ($('.box').height() > $(window).height() || $('.box').width() > $(window).width()) {
+    //checks box size when user resizes window
+    if ($('.box').height() > $(window).height() || $('.box').width() > $(window).width()) {
 
-        $('.box').css({
-          "width": "400px",
-          "height": "auto"
-        });
-      }
-    
-    });
+      $('.box').css({
+        "width": "400px",
+        "height": "auto"
+      });
+    }
 
+  });
 
   //lightbox only works on screens above 410px, reference: http://www.tonylea.com/2011/how-to-create-your-own-jquery-lightbox/
-  if ($(window).width() > 410 ) {
+  if ($(window).width() > 410) {
+    if (/iP(hone|od)|android.+mobile|BlackBerry|IEMobile/i.test(navigator.userAgent) || /(tablet|ipad|playbook|silk)|(android(?!.*mobile))/i.test(navigator.userAgent)) {
+      //if tablet or smartphone do nothing
+    } else {
+      //effects on images when hovering , not used in about and email box because of resizing issues
+      $('.grid-item .img, .socialIcon').hover(function() {
+        $(this).toggleClass('animated pulse');
+      });
 
-    //lightbox items on click function
-    $('.img').click(function() {
-      //makes images bigger instead of using lightbox
-      //if ($(window).width() < 620 ){
-        $( this ).parent('.grid-item').toggleClass('expandSmall');
+      //lightbox items on click function
+      $('.img').click(function() {
+        //makes images bigger instead of using lightbox
+        //if ($(window).width() < 620 ){
+        $(this).parent('.grid-item').toggleClass('expandSmall');
         $grid.masonry();
         /*
-      }else{
-        $( this ).parent('.grid-item').toggleClass('expand');
-        $grid.masonry();
-      }
-    */
-  /* currently blocked out is the lighbox settings using expand class above instead that enlarges images
+          }else{
+            $( this ).parent('.grid-item').toggleClass('expand');
+            $grid.masonry();
+          }
+        */
+        /* currently blocked out is the lighbox settings using expand class above instead that enlarges images
 
-      $('body').append('<div onclick="close_box()" class="backdrop"></div><img onclick="close_box()" class="box" src="' + $(this).attr("src") + '"></div>');
-      $('.backdrop').css('height', '' + $(document).height() + 'px');
+            $('body').append('<div onclick="close_box()" class="backdrop"></div><img onclick="close_box()" class="box" src="' + $(this).attr("src") + '"></div>');
+            $('.backdrop').css('height', '' + $(document).height() + 'px');
 
-      //get naturalHeight and naturalWidth of image reference: https://css-tricks.com/snippets/jquery/get-an-images-native-width/
-      // Get on screen image
-      var screenImage = $(this);
+            //get naturalHeight and naturalWidth of image reference: https://css-tricks.com/snippets/jquery/get-an-images-native-width/
+            // Get on screen image
+            var screenImage = $(this);
 
-      // Create new offscreen image to test
-      var theImage = new Image();
-      theImage.src = screenImage.attr("src");
+            // Create new offscreen image to test
+            var theImage = new Image();
+            theImage.src = screenImage.attr("src");
 
-      // Get original/accurate measurements from images.
-      var imageWidth = theImage.width;
-      var imageHeight = theImage.height;
-      
+            // Get original/accurate measurements from images.
+            var imageWidth = theImage.width;
+            var imageHeight = theImage.height;
+            
 
-        //fixes "biggers than window" images by checking original width to window width
-      if ($(window).width() < imageWidth ) {
-        $('.box').css({
-          "width": "auto",
-          "height": "80%"
-        });
-      }
+              //fixes "biggers than window" images by checking original width to window width
+            if ($(window).width() < imageWidth ) {
+              $('.box').css({
+                "width": "auto",
+                "height": "80%"
+              });
+            }
 
-      //css for lightbox
-      $('.backdrop').animate({
-        'opacity': '.50'
-      }, 300, 'linear');
-      $('.box').animate({
-        'opacity': '1.00'
-      }, 300, 'linear');
-      $('.backdrop, .box').css('display', 'block');
-      
+            //css for lightbox
+            $('.backdrop').animate({
+              'opacity': '.50'
+            }, 300, 'linear');
+            $('.box').animate({
+              'opacity': '1.00'
+            }, 300, 'linear');
+            $('.backdrop, .box').css('display', 'block');
+            
 
-      //checks if css settings prevent "bigger than window" image, else use different width 
-      if ($('.box').height() > $(window).height() || $('.box').width() > $(window).width()) {
+            //checks if css settings prevent "bigger than window" image, else use different width 
+            if ($('.box').height() > $(window).height() || $('.box').width() > $(window).width()) {
 
-        $('.box').css({
-          "width": "400px",
-          "height": "auto"
-        });
-      }
-  //end of lightbox settings
+              $('.box').css({
+                "width": "400px",
+                "height": "auto"
+              });
+            }
+        //end of lightbox settings
 
 
-      */
-    });//end of img click function
+            */
+      }); //end of img click function
 
-  }//end of if (screen > 410) statement 
+    } //end of tablet and smartphone detection
 
-//start of, clicking on about box changes
+  } //end of if (screen > 410) statement 
+
+  //start of, clicking on about box changes
 
   var aboutText = "Residing in Phoenix, I am a current student of Arizona State University at the Herberger Institute of Design. Being 23 years young, I strive to expand on what I can do creatively as I mainly working with mix media, which includes, but not limited to, watercolors, graphite, ink, and markers. I emphasize my work mainly on human form, focusing on those in my life and around me. My medium targets color and manipulation of the human form with environments around my subjects.";
 
-  //effects on images when hovering , not used in about and email box because of resizing issues
-  $('.grid-item .img, .socialIcon').hover(function(){
-    $(this).toggleClass('animated pulse');
-  });
-
   $('#about').on('click', function() {
-  //add paddding to text not box 
+    //add paddding to text not box 
 
-  //toggle main class creates a bigger div box
-  $(this).toggleClass('main');
-  $('#aboutText').toggleClass('animated fadeInDown aboutMargin');
+    //toggle main class creates a bigger div box
+    $(this).toggleClass('main');
+    $('#aboutText').toggleClass('animated fadeInDown aboutMargin');
 
-  //checking text in aboutText, if empty add text. Used for toggle
-  $("#aboutText").text($("#aboutText").text() == aboutText ? '' : aboutText);
-  $grid.masonry();
+    //checking text in aboutText, if empty add text. Used for toggle
+    $("#aboutText").text($("#aboutText").text() == aboutText ? '' : aboutText);
+    $grid.masonry();
 
-  });//end of click
+  }); //end of click
 
-//end of aboutText click function
+  //end of aboutText click function
 
 }); //end of document ready
-
-
 
 function close_box() {
   $('.backdrop, .box').animate({
@@ -260,4 +209,3 @@ function close_box() {
     $('.backdrop, .box').remove();
   });
 }
-
